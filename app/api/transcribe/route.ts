e91062da17179ef3e7e553d24475f81129f98db7
hassanalkhalaf <hassanalkhalaf2015@gmail.com>
2026-08-04 03:13:57 +0300

    Update route.ts

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -145,8 +145,7 @@ export async function POST(req: NextRequest) {
 
   try {
     const dgUrl =
-      "https://api.deepgram.com/v1/listen?model=nova-2&language=multi&smart_format=true&punctuate=true";
-
+"https://api.deepgram.com/v1/listen?model=nova-2&language=ar&smart_format=true&punctuate=true";
     const response = await fetch(dgUrl, {
       method: "POST",
       headers: {
